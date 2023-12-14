ab03f0ee7f79c040043a6a3f0c0a01e7945209cc
trevor <thisistrevor4@gmail.com>
2023-12-13 20:43:48 -0600

    add package levels

--- app/src/main/java/com/trevorwiebe/stackoverflowreader/MainActivity.kt
+++ app/src/main/java/com/trevorwiebe/stackoverflowreader/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.trevorwiebe.stackoverflowreader.ui.theme.StackOverflowReaderTheme
+import com.trevorwiebe.stackoverflowreader.presentation.ui.theme.StackOverflowReaderTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
