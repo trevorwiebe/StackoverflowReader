1d28afebf14a1d44176a5a41ba1115ae274353f4
trevor <thisistrevor4@gmail.com>
2023-12-18 21:37:44 -0600

    add hiltAndroidApp

---
+++ app/src/main/java/com/trevorwiebe/stackoverflowreader/HotQuestionsApp.kt
@@ -0,0 +1,8 @@
+package com.trevorwiebe.stackoverflowreader
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HotQuestionsApp : Application() {
+}
