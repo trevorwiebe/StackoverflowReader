3a022aa5ece317bf09c95e64e8bf9ccdc750466f
trevor <thisistrevor4@gmail.com>
2023-12-23 15:03:14 -0600

    add top app bar

--- app/src/main/java/com/trevorwiebe/stackoverflowreader/presentation/MainActivity.kt
+++ app/src/main/java/com/trevorwiebe/stackoverflowreader/presentation/MainActivity.kt
@@ -3,7 +3,20 @@ package com.trevorwiebe.stackoverflowreader.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -19,23 +32,44 @@ const val TAG = "MainActivity"
 @AndroidEntryPoint
 class MainActivity : ComponentActivity(){
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StackOverflowReaderTheme {
-                HotQuestionsApp()
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.background,
+                                titleContentColor = MaterialTheme.colorScheme.onBackground,
+                            ),
+                            title = {
+                                Text("Stackoverflow Reader")
+                            },
+                            actions = {
+                                IconButton(onClick = {  }) {
+                                    Icon(Icons.Filled.Settings, contentDescription = "Settings")
+                                }
+                            }
+                        )
+                    }
+                ) {
+                    HotQuestionsApp(it)
+                }
             }
         }
     }
 
     @Composable
-    fun HotQuestionsApp() {
+    fun HotQuestionsApp(innerPadding: PaddingValues) {
 
         val navController = rememberNavController()
 
         NavHost(
             navController = navController,
-            startDestination = HotQuestionsListDestination.route
+            startDestination = HotQuestionsListDestination.route,
+            modifier = Modifier.padding(innerPadding)
         ){
             composable(route = HotQuestionsListDestination.route){
                 HotQuestions(
