0e87bc74d97bff96c1684951b89b52cb7d3238f4
trevor <thisistrevor4@gmail.com>
2023-12-23 12:59:57 -0600

    Add string file

---
+++ app/src/main/java/com/trevorwiebe/stackoverflowreader/data/util/Utility.kt
@@ -0,0 +1,5 @@
+package com.trevorwiebe.stackoverflowreader.data.util
+
+const val BASE_URL = "https://stackexchange.com/"
+const val BASE_API_URL = "https://api.stackexchange.com/2.3/"
+const val FILTER = "!u.*0VeqBWiJrWzd1n5JTZcq7D9h_ZtQ"
