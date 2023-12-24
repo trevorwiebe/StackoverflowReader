6d8533fd67fd8a88145117c858799b345976b1b7
trevor <thisistrevor4@gmail.com>
2023-12-24 08:17:09 -0600

    change arrayList to list

--- app/src/main/java/com/trevorwiebe/stackoverflowreader/data/dto/ComplexQuestionDto.kt
+++ app/src/main/java/com/trevorwiebe/stackoverflowreader/data/dto/ComplexQuestionDto.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class ComplexQuestionDto (
 
-    @SerializedName("items") var items : ArrayList<Items> = arrayListOf(),
+    @SerializedName("items") var items : List<Items> = emptyList(),
     @SerializedName("has_more") var hasMore : Boolean? = null,
     @SerializedName("quota_max") var quotaMax : Int? = null,
     @SerializedName("quota_remaining") var quotaRemaining : Int? = null
